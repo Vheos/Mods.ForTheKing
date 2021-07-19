5715e147124402cb09844dd46a5599d08d17511b
Vheos <Vheos777@gmail.com>
2021-07-19 23:03:55 +0200

    added test cheat mods

---
+++ Mods/Cheats.cs
@@ -0,0 +1,30 @@
+﻿namespace Vheos.Mods.ForTheKing
+{
+    using HarmonyLib;
+    using Vheos.Tools.ModdingCore;
+    public class Cheats : AMod
+    {
+        // Setting
+        static private ModSetting<bool> _alwaysMaxHealth, _alwaysMaxFocus;
+        override protected void Initialize()
+        {
+            _alwaysMaxHealth = CreateSetting(nameof(_alwaysMaxHealth), false);
+            _alwaysMaxFocus = CreateSetting(nameof(_alwaysMaxFocus), false);
+        }
+        override protected void SetFormatting()
+        {
+            _alwaysMaxHealth.Format("Always max health");
+            _alwaysMaxFocus.Format("Always max focus");
+        }
+
+        // Hooks
+        [HarmonyPatch(typeof(CharacterStats), "Update"), HarmonyPrefix]
+        static void CharacterStats_Update_Pre(CharacterStats __instance)
+        {
+            if (_alwaysMaxHealth)
+                __instance.m_HealthCurrent = __instance.MaxHealth;
+            if (_alwaysMaxFocus)
+                __instance.m_FocusPoints = __instance.MaxFocus;
+        }
+    }
+}
