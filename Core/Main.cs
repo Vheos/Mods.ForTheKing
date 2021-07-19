42cba6a2bc8fc604f6a20c40c9a11b8fbd704e40
Vheos <Vheos777@gmail.com>
2021-07-19 22:08:40 +0200

    Initial commit

---
+++ Core/Main.cs
@@ -0,0 +1,24 @@
+﻿namespace Vheos.Mods.ForTheKing
+{
+    using System;
+    using System.Reflection;
+    using BepInEx;
+    using Vheos.Tools.ModdingCore;
+    [BepInDependency("com.bepis.bepinex.configurationmanager", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Main : BepInExEntryPoint
+    {
+        #region SETTINGS
+        public const string GUID = "Vheos.Mods.ForTheKing";
+        public const string NAME = "Vheos Mod Pack";
+        public const string VERSION = "0.1.0";
+        #endregion
+
+        // User logic
+        override public Assembly CurrentAssembly
+        => Assembly.GetExecutingAssembly();
+        override public void Initialize()
+        {
+        }
+    }
+}
---
+++ Mods/Targeting.cs
@@ -0,0 +1,22 @@
+﻿namespace Vheos.Mods.ForTheKing
+{
+    using Vheos.Tools.ModdingCore;
+    public class TestMod : AMod
+    {
+        // Setting
+        static private ModSetting<bool> _testBool;
+        static private ModSetting<float> _testFloat;
+        override protected void Initialize()
+        {
+            _testBool = CreateSetting(nameof(_testBool), false);
+            _testFloat = CreateSetting(nameof(_testFloat), 0f, FloatRange(0f, 100f));
+        }
+        override protected void SetFormatting()
+        {
+            _testBool.Format("Test bool");
+            _testFloat.Format("Test float", _testBool);
+        }
+        override protected string Description
+        => "Description test";
+    }
+}
