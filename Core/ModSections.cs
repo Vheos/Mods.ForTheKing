5304fcf25b4340a6f944d54f3f7e54ae4e941770
Vheos <Vheos777@gmail.com>
2021-07-22 20:17:04 +0200

    added ModSections

---
+++ Core/ModSections.cs
@@ -0,0 +1,8 @@
+﻿namespace Vheos.Mods.ForTheKing
+{
+    static public class ModSections
+    {
+        static public string Rebalance = new string(' ', 1) + "REBALANCE";
+        static public string Development = new string(' ', 0) + "DEVELOPMENT";
+    }
+}
