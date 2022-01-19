d70510f60cf781dfc873bef294600af1b3bd3cad
Vheos <Vheos777@gmail.com>
2022-01-19 14:44:03 +0100

    cleaned up ignores

---
+++ Code/Mods/Cheats.cs
@@ -0,0 +1,69 @@
+﻿namespace Vheos.Mods.ForTheKing
+{
+    using HarmonyLib;
+    using Mods.Core;
+    using Tools.Extensions.Math;
+    public class Cheats : AMod
+    {
+        // Setting
+        static private ModSetting<int> _setHealth, _setFocus, _setGold;
+        static private ModSetting<SetStatsFrequency> _setStatsFrequency;
+        override protected void Initialize()
+        {
+            _setHealth = CreateSetting(nameof(_setHealth), 200, IntRange(0, 200));
+            _setFocus = CreateSetting(nameof(_setFocus), 10, IntRange(0, 10));
+            _setGold = CreateSetting(nameof(_setGold), 2000, IntRange(0, 2000));
+            _setStatsFrequency = CreateSetting(nameof(_setStatsFrequency), SetStatsFrequency.Never);
+
+            ConfigHelper.AddEventOnConfigClosed(() =>
+            {
+                if (_setStatsFrequency == SetStatsFrequency.Once
+                || _setStatsFrequency == SetStatsFrequency.OnConfigClosed)
+                    foreach (var character in FTKHub.Instance.m_CharacterOverworlds)
+                    {
+                        SetStats(character.m_CharacterStats);
+                        if (_setStatsFrequency == SetStatsFrequency.Once)
+                            _setStatsFrequency.Value = SetStatsFrequency.Never;
+                    }
+            });
+        }
+        override protected void SetFormatting()
+        {
+            _setStatsFrequency.Format("Set stats");
+            using(Indent)
+            {
+                _setHealth.Format("Health", _setStatsFrequency, SetStatsFrequency.Never, false);
+                _setFocus.Format("Focus", _setStatsFrequency, SetStatsFrequency.Never, false);
+                _setGold.Format("Gold", _setStatsFrequency, SetStatsFrequency.Never, false);
+            }
+        }
+        override protected string SectionOverride
+        => ModSections.Development;
+
+        // Logic
+#pragma warning disable IDE0051, IDE0060, IDE1006
+
+        #region Set stats
+        private enum SetStatsFrequency
+        {
+            Never = 0,
+            Once,
+            OnConfigClosed,
+            OnUpdate,
+        }
+        static private void SetStats(CharacterStats stats)
+        {
+            stats.m_HealthCurrent = _setHealth.Value.ClampMax(stats.MaxHealth);
+            stats.m_FocusPoints = _setFocus.Value.ClampMax(stats.MaxFocus);
+            stats.m_Gold = _setGold;
+        }
+
+        [HarmonyPatch(typeof(CharacterStats), "Update"), HarmonyPrefix]
+        static private void CharacterStats_Update_Pre(CharacterStats __instance)
+        {
+            if (_setStatsFrequency == SetStatsFrequency.OnUpdate)
+                SetStats(__instance);
+        }
+        #endregion
+    }
+}
---
+++ Code/Mods/Debug.cs
@@ -0,0 +1,38 @@
+﻿namespace Vheos.Mods.ForTheKing
+{
+    using System;
+    using System.Collections.Generic;
+    using HarmonyLib;
+    using UnityEngine;
+    using Mods.Core;
+    using Tools.Extensions.General;
+    using Tools.Extensions.DumpN;
+    using Tools.UtilityN;
+    using Tools.Extensions.UnityObjects;
+    using GridEditor;
+    public class Debug : AMod, IUpdatable
+    {
+        // Setting
+        override protected void Initialize()
+        {
+        }
+        override protected void SetFormatting()
+        {
+        }
+        public void OnUpdate()
+        {
+            if (KeyCode.Keypad0.Pressed())
+            {
+                Log.Debug($"ID\t{   typeof(FTK_weaponStats2).Dump(null, MemberData.Names) }");
+                Log.Debug($"enum\t{  typeof(FTK_weaponStats2).Dump(null, MemberData.Types) }");
+                foreach (var itemID in Utility.GetEnumValues<FTK_itembase.ID>())
+                    if (FTK_weaponStats2DB.Get(itemID).TryNonNull(out var weaponStats))
+                        Log.Debug($"{itemID}\t{weaponStats.Dump(typeof(FTK_weaponStats2))}");
+            }
+        }
+        override protected string SectionOverride
+        => ModSections.Development;
+
+        // Hooks
+    }
+}
